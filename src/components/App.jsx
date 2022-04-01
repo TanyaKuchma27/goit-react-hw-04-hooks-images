597de4cb253760dbe1334db463ec72c5488cdbbd
Tanya Kuchma <kuchma.tanya@ukr.net>
2022-04-01 16:26:28 +0300

    refactor Searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+// import { useState } from 'react';
 import Searchbar from './Searchbar';
 import ImageGallery from './ImageGallery';
 import Button from './Button';
@@ -9,6 +9,101 @@ import 'react-toastify/dist/ReactToastify.css';
 import photoAPI from '../services/photoApi';
 import {Container} from './App.styled';
 
+// function App() {
+//   const [photo, setPhoto] = useState('');
+//   const [result, setResult] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [page, setPage] = useState(1);
+//   const [modalPhoto, setModalPhoto] = useState('');
+//   const [showModal, setShowModal] = useState(false);
+//   const [end, setEnd] = useState(false);
+ 
+  // async componentDidUpdate(prevProps, prevState) {
+  //   const {photo} = this.state;
+  //   const prevPhoto = prevState.photo;    
+
+  //   if (photo !== prevPhoto) {
+  //     this.setState({ isLoading: true, page: 1 });
+  //     try {
+  //       const result = await photoAPI.fetchPhoto(photo);
+  //       const photos = result.hits;
+  //       const totalHits = result.totalHits;
+  //       const maxPage = Math.ceil(totalHits / 12);        
+
+  //       if (totalHits === 0) {
+  //         toast.error('Sorry, there are no images matching your search query. Please try again.');
+  //         this.setState({isLoading: false });
+  //         return;
+  //       };
+                
+  //       if (maxPage === 1) {
+  //         this.setState({ result: photos, isLoading: false, end: true });
+  //         toast("We're sorry, but you've reached the end of search results.");
+  //         return
+  //       }
+
+  //       this.setState({ result: photos, isLoading: false, end: false });
+  //     } catch(error) {
+  //         console.log(error);
+  //       }     
+  //   }
+  // }
+
+  // const handleFormSubmit = photo => {
+  //   setPhoto(photo);
+  // };
+
+  // handleLoadMore = async() => {
+  //   this.setState({
+  //     isLoading: true,
+  //     page: this.state.page + 1
+  //   })
+
+  //   try {
+  //     const result = await photoAPI.fetchPhoto(this.state.photo, this.state.page + 1);
+  //     const photos = result.hits;
+  //     const totalHits = result.totalHits;
+  //     const maxPage = Math.ceil(totalHits / 12);
+
+  //     if (maxPage === this.state.page) {
+  //       this.setState({ result: [...this.state.result, ...photos], isLoading: false, end: true });
+  //       toast("We're sorry, but you've reached the end of search results.");
+  //       return;          
+  //     }
+
+  //     this.setState({ result: [...this.state.result, ...photos], isLoading: false });
+  //   } catch (error) {
+  //     console.log(error);
+  //     }
+  // };
+
+//   const updateModalPhoto = modalPhoto => {
+//     setModalPhoto(modalPhoto);
+//   };
+
+//   const toggleModal = () => {
+//     setShowModal(!showModal);
+//   };
+
+//   return (
+//     <Container>
+//       <Searchbar onSubmit={handleFormSubmit} />
+//       <ImageGallery
+//         result={result}
+//         setModalPhoto={updateModalPhoto}
+//         openModal={toggleModal}
+//       />
+//       {result.length > 0 && !isLoading && !end && <Button onClick={handleLoadMore}/>}
+//       {isLoading && <Loader />}
+//       {showModal && <Modal photo={photo} modalPhoto={modalPhoto} onClose={toggleModal}/>}
+//       <ToastContainer autoClose={3000} />
+//     </Container>
+//   );
+// };
+
+// export default App;
+
+import React, { Component } from 'react';
 class App extends Component {
   state = {
     photo: '',
@@ -94,7 +189,7 @@ class App extends Component {
 
     return (
       <Container>
-        <Searchbar onSubmit={this.handleFormSubmit} />
+        <Searchbar onSubmit={this.handleFormSubmit} />        
         <ImageGallery
           result={result}
           setModalPhoto={this.setModalPhoto}
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,58 +1,103 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import {Header, Form, Button, ButtonLabel, Input} from './Searchbar.styled';
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { toast } from 'react-toastify';
 import PropTypes from 'prop-types';
 
-
-class Searchbar extends Component {
-    static propTypes = {
-        onSubmit: PropTypes.func.isRequired,
-    };
+function Searchbar({ onSubmit }) {
+    const [photo, setPhoto] = useState('');
     
-    state = {
-        photo: '',
+    const handlePhotoChange = event => {
+        setPhoto(event.currentTarget.value.toLowerCase().trim());           
     };
 
-    handlePhotoChange = event => {
-        this.setState({ photo: event.currentTarget.value.toLowerCase() });          
-    };
-
-    handleSubmit = event => {
+    const handleSubmit = event => {
         event.preventDefault();
 
-        if (this.state.photo.trim() === '') {
+        if (photo.trim() === '') {
             toast.error('Please, enter data');        
             return;
-        }
-
-        this.props.onSubmit(this.state.photo);
-        this.setState({ photo: '' });
+        }    
+        onSubmit(photo);
+        setPhoto('');
     };
 
-    render() {
-        return (
-            <Header>
-                <Form onSubmit={this.handleSubmit}>
-                    <Button type="submit">
-                        <FontAwesomeIcon icon={faMagnifyingGlass} style={{ fontSize: 20 }}/>
-                        <ButtonLabel>Search</ButtonLabel>
-                    </Button>
-
-                    <Input
-                        type="text"
-                        name="photo"
-                        value={this.state.photo}
-                        onChange={this.handlePhotoChange}
-                        autoComplete="off"
-                        autoFocus
-                        placeholder="Search images and photos"
-                    />
-                </Form>
-            </Header>
-        );
-    }
+    return (
+        <Header>
+            <Form onSubmit={handleSubmit}>
+                <Button type="submit">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} style={{ fontSize: 20 }}/>
+                    <ButtonLabel>Search</ButtonLabel>
+                </Button>
+                <Input
+                    type="text"
+                    name="photo"
+                    value={photo}
+                    onChange={handlePhotoChange}
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                />
+            </Form>
+        </Header>
+    );    
 }
 
+Searchbar.propTypes = {
+    onSubmit: PropTypes.func.isRequired,   
+};
+
 export default Searchbar;
+
+// import React, { Component } from 'react';
+// class Searchbar extends Component {
+//     static propTypes = {
+//         onSubmit: PropTypes.func.isRequired,
+//     };
+    
+//     state = {
+//         photo: '',
+//     };
+
+//     handlePhotoChange = event => {
+//         this.setState({ photo: event.currentTarget.value.toLowerCase() });          
+//     };
+
+//     handleSubmit = event => {
+//         event.preventDefault();
+
+//         if (this.state.photo.trim() === '') {
+//             toast.error('Please, enter data');        
+//             return;
+//         }
+
+//         this.props.onSubmit(this.state.photo);
+//         this.setState({ photo: '' });
+//     };
+
+//     render() {
+//         return (
+//             <Header>
+//                 <Form onSubmit={this.handleSubmit}>
+//                     <Button type="submit">
+//                         <FontAwesomeIcon icon={faMagnifyingGlass} style={{ fontSize: 20 }}/>
+//                         <ButtonLabel>Search</ButtonLabel>
+//                     </Button>
+
+//                     <Input
+//                         type="text"
+//                         name="photo"
+//                         value={this.state.photo}
+//                         onChange={this.handlePhotoChange}
+//                         autoComplete="off"
+//                         autoFocus
+//                         placeholder="Search images and photos"
+//                     />
+//                 </Form>
+//             </Header>
+//         );
+//     }
+// }
+
+// export default Searchbar;
